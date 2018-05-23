a9c9eb7187e5f9e8fb1bb8c2279331e4222c38ad
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-23 23:25:03 +0200

    add first React component test

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,22 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from '../login';
+
 // Basic unit test
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
   // create a fake object to hold the form field values (username and password)
+  const fakeUser = {
+    username: 'kaktus',
+    password: 'kaktus'
+  };
   // create a jest.fn() for your submit handler
+  const fakeSubmit = jest.fn();
   // render the Login component to a div
+  const div = document.createElement('div');
+  ReactDOM.render(<Login onSubmit={fakeSubmit} />, div);
   // TIP: const div = document.createElement('div')
   //
   // get the field nodes
   // TIP: const inputs = div.querySelectorAll('input')
+  // const login = div.querySelector('input[name=username]');
+  // const password = div.querySelector('input[name=password]');
+  const form = div.querySelector('form');
+  const { username, password } = form.elements;
   // TIP: const form = div.querySelector('form')
   // fill in the field values
+  username.value = fakeUser.username;
+  password.value = fakeUser.password;
   //
   // Act
   // submit the form:
+  const submitEvent = new window.Event('submit');
+  form.dispatchEvent(submitEvent);
   // TIP: formNode.dispatchEvent(new window.Event('submit'))
   //
   // Assert
   // ensure your submit handler was called properly
+  expect(fakeSubmit).toHaveBeenCalledTimes(1);
+  expect(fakeSubmit).toHaveBeenCalledWith(fakeUser);
+
 })
 
 //////// Elaboration & Feedback /////////
@@ -28,8 +50,8 @@ test('calls onSubmit with the username and password when submitted', () => {
 /*
 http://ws.kcd.im/?ws=Testing&e=login.step-1&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
